8fc1b17c8abe38edcfe236c7a81819c104edea71
Sharon Li <sl2495@cornell.edu>
2020-07-14 16:26:13 -0500

    changed all pages to html

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <ul>
-        <li><a href="mailto: sl2495@cornell.edu">Email</a></li>
-        <li><a href="https://medium.com/@sharonli_75078">Medium</a></li>
-        <li><a href="https://www.linkedin.com/in/sharon-li-966966168/">LinkedIn</a></li>
-    </ul>
-    <img src="images/icons/flower-icon.png" alt="Flower Icon"/>
-    <!-- Icon owned by me -->
-</footer>
--- includes/head.php
+++
@@ -1,6 +0,0 @@
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Sharon's Portfolio</title>
-  <link rel="stylesheet" type="text/css" href="styles/all.css" />
-</head>
